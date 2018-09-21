bb7207521b2ca31a040a09202952bfbeb1d1bee5
MMPark <parkmee@gmail.com>
2018-09-21 01:25:40 -0400

    started animation logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,57 +16,56 @@ $(document).ready(function (){
 
     // declare global variables
     const btnView = $("#btn-view");
+    const animalInput = $("#animal-input");
     let indexPos = 0;
-    let term = "";
 
     for (var i in animals) {
         // populate screen with buttons
         createBtn();
     }
 
+    $("#submit-btn").on("click", function() {
+        animals.push(animalInput.val().trim().toLowerCase());
+        createBtn();
+    });
+
     function createBtn () {
         // add button to screen
-        btnView.append(
-            `
-                <button class="btn btn-info mt-2 mr-2 shadow term-btn">${animals[indexPos]}</button>
-            `
-        );
+        btnView.append(`<button class="btn btn-info mt-2 mr-2 shadow term-btn">${animals[indexPos]}</button>`);
         // increment number of index position number in array
         indexPos++;
     }
+});
 
-    $(".term-btn").on("click", function(){
-        console.log($(this).text());
-        term = $(this).text();
+$(document).on("click", ".term-btn", function(){
+    let term = $(this).text();
 
-        // define queryURL
-        const queryURL = `https://api.giphy.com/v1/gifs/search?q=${term}&api_key=d2owZB648qOVga3aNLKfn3RhLhTHYVo2&limit=10`;
+    // define queryURL
+    const queryURL = `https://api.giphy.com/v1/gifs/search?q=${term}&api_key=d2owZB648qOVga3aNLKfn3RhLhTHYVo2&limit=10`;
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
-            let j = 0;
-            for (var i = 0; i < 10; i++) {
-                $("#gif-view").prepend(
-                    `
-                        <div class="d-inline-block">
-                            <p class="m-2"><strong>Rating: </strong>${response.data[j].rating}</p>
-                            <img class="m-2k" src="${response.data[j].images.fixed_height.url}"/">
-                        </div>
-                    `
-                );
-                j++;
-            };
-            
-        });
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        let j = 0;
+        for (var i = 0; i < 10; i++) {
+            $("#gif-view").prepend(
+                `
+                    <div class="d-inline-block">
+                        <p class="m-2"><strong>Rating: </strong>${response.data[j].rating}</p>
+                        <img class="m-2k" img-state="still" src="${response.data[j].images.fixed_height_still.url}"/">
+                    </div>
+                `
+            );
+            j++;
+        };
+        
     });
+});
 
-    
-    
-    
-
-
-
+$(document).on("click", "img", function() {
+    let imgState = $(this).attr("img-state");
+    console.log($(this).attr("src"));
+    console.log($(this).attr("img-state"));
 });
