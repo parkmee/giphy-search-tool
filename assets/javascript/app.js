c7929aa06daa4a64e8285ee7b22d337759f0528d
MMPark <parkmee@gmail.com>
2018-09-22 16:58:25 -0400

    updated theme and mechanics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,52 @@
 $(document).ready(function (){
 
-    // populate initial list of animals to search gifs for
-    let animals = [
-        "cat", 
-        "dog", 
-        "monkey", 
-        "chipmunk", 
-        "dolphin", 
-        "manatee", 
-        "panda",
-        "bear",
-        "squirrel",
-        "trash panda",
+    // populate initial list of games to search gifs for
+    let games = [
+        "mario kart", 
+        "final fantasy", 
+        "donkey kong", 
+        "super mario bros", 
+        "mortal kombat", 
+        "pac man", 
+        "zelda",
+        "double dragon II",
+        "lego star wars",
+        "contra",
     ];
 
     // declare global variables
     const btnView = $("#btn-view");
-    const animalInput = $("#animal-input");
+    const animalInput = $("#game-input");
     let indexPos = 0;
 
-    for (var i in animals) {
-        // populate screen with buttons
-        createBtn();
+    createBtns();
+
+    function createBtns () {
+
+        $("#btn-view").empty();
+        indexPos = 0;
+
+        for (var i in games) {
+            // populate screen with buttons
+            games = games.sort();
+            // add button to screen
+            btnView.append(`<button class="btn btn-info mt-2 mr-2 shadow term-btn">${games[indexPos]}</button>`);
+            // increment number of index position number in array
+            indexPos++;
+        }
     }
+    
 
     $("#submit-btn").on("click", function() {
-        animals.push(animalInput.val().trim().toLowerCase());
-        createBtn();
+
+        if (gameInput.val() === "" ) {
+            console.log("empty input");
+        } else {
+            games.push(gameInput.val().trim().toLowerCase());
+            createBtns();
+        }
     });
 
-    function createBtn () {
-        // add button to screen
-        btnView.append(`<button class="btn btn-info mt-2 mr-2 shadow term-btn">${animals[indexPos]}</button>`);
-        // increment number of index position number in array
-        indexPos++;
-    }
 });
 
 $(document).on("click", ".term-btn", function(){
@@ -52,15 +64,18 @@ $(document).on("click", ".term-btn", function(){
         for (var i = 0; i < 10; i++) {
             $("#gif-view").prepend(
                 `
-                    <div class="d-inline-block">
-                        <p class="m-2"><strong>Rating: </strong>${response.data[j].rating}</p>
-                        <img class="m-2k" img-state="still" src="${response.data[j].images.fixed_height_still.url}"/">
+                    <div class="d-inline-block border border-dark rounded">
+                        <img class="gif-tile" img-state="still" 
+                            src="${response.data[j].images.fixed_height_still.url}"
+                            img-still="${response.data[j].images.fixed_height_still.url}"
+                            img-animate="${response.data[j].images.fixed_height.url}">
+                        <span class="d-block text-center bg-dark rating">RATING: ${response.data[j].rating.toUpperCase()}</span>
+
                     </div>
                 `
             );
             j++;
         };
-        
     });
 });
 
@@ -68,4 +83,13 @@ $(document).on("click", "img", function() {
     let imgState = $(this).attr("img-state");
     console.log($(this).attr("src"));
     console.log($(this).attr("img-state"));
+    if (imgState === "still") {
+        const imgAnimate = $(this).attr("img-animate");
+        $(this).attr("src", imgAnimate); 
+        $(this).attr("img-state", "animate");
+    } else {
+        const imgStill = $(this).attr("img-still");
+        $(this).attr("src", imgStill);
+        $(this).attr("img-state", "still");
+    }
 });
