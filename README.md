228fcfbd095e38b51442b055f94d7408ef6905c7
MMPark <parkmee@gmail.com>
2018-09-24 11:02:54 -0400

    updated formatting and limited gif sizing

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # giphy-search-tool
 
 Add new search terms as buttons and click to load gifs
-Click on stills to animate
+Click on still images to animate and vice versa
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,22 +12,27 @@ $(document).ready(function (){
         "double dragon II",
         "lego star wars",
         "contra",
+        "zero horizon dawn",
+        "assasin's creed",
     ];
 
     // declare global variables
     const btnView = $("#btn-view");
-    const animalInput = $("#game-input");
+    const gameInput = $("#game-input");
     let indexPos = 0;
 
+    // run create button function at page load
     createBtns();
 
     function createBtns () {
 
+        // empty btn-view div of all buttons and reset index position counter to 0
         $("#btn-view").empty();
         indexPos = 0;
 
-        for (var i in games) {
-            // populate screen with buttons
+        // for every item in the games array
+        for (let i in games) {
+            // sort games alphabetically
             games = games.sort();
             // add button to screen
             btnView.append(`<button class="btn btn-info mt-2 mr-2 shadow term-btn">${games[indexPos]}</button>`);
@@ -36,46 +41,61 @@ $(document).ready(function (){
         }
     }
     
-
+    // on click of submit button
     $("#submit-btn").on("click", function() {
 
+        // if input is null, log statement in console
         if (gameInput.val() === "" ) {
             console.log("empty input");
+        
+        // otherwise, trim and push lowercase string of value into games array and launch function to create buttons
         } else {
             games.push(gameInput.val().trim().toLowerCase());
             createBtns();
         }
     });
-
 });
 
+// on click of a dynamically created button
 $(document).on("click", ".term-btn", function(){
+
+    // set term to equal the text on the button
     let term = $(this).text();
 
     // define queryURL
-    const queryURL = `https://api.giphy.com/v1/gifs/search?q=${term}&api_key=d2owZB648qOVga3aNLKfn3RhLhTHYVo2&limit=10`;
+    const queryURL = `https://api.giphy.com/v1/gifs/search?q=${term}&api_key=d2owZB648qOVga3aNLKfn3RhLhTHYVo2&limit=30`;
 
+    // run ajax query to get gif from gipgy
     $.ajax({
         url: queryURL,
         method: "GET"
+
+    // after receiving response, for each item in response object, create an image tile with a static picture in still state
     }).then(function(response){
         console.log(response);
-        let j = 0;
-        for (var i = 0; i < 10; i++) {
-            $("#gif-view").prepend(
-                `
-                    <div class="d-inline-block border border-dark rounded">
-                        <img class="gif-tile" img-state="still" 
-                            src="${response.data[j].images.fixed_height_still.url}"
-                            img-still="${response.data[j].images.fixed_height_still.url}"
-                            img-animate="${response.data[j].images.fixed_height.url}">
-                        <span class="d-block text-center bg-dark rating">RATING: ${response.data[j].rating.toUpperCase()}</span>
-
-                    </div>
-                `
-            );
-            j++;
-        };
+        let i = 0;
+        for (var j = 0; j < 30; j++) {
+            const width = parseInt(response.data[j].images.fixed_height_still.width);
+            if (width > 230 && width < 375 && i < 10) {
+                const stillURL = response.data[j].images.fixed_height_still.url;
+                const animateURL = response.data[j].images.fixed_height.url;
+                const title = response.data[j].title.replace(" GIF","").toUpperCase();
+                const rating = response.data[j].rating.toUpperCase();
+                
+                $("#gif-view").prepend(
+                    `
+                        <div class="d-inline-block border border-dark rounded mt-2 ml-2 shadow">
+                            <img class="gif-tile" img-state="still" 
+                                src="${stillURL}"
+                                img-still="${stillURL}"
+                                img-animate="${animateURL}">
+                            <span class="d-block text-center bg-dark rating">${title}<br>RATING: ${rating}</span>
+                        </div>
+                    `
+                );
+                i++;
+            }
+        }
     });
 });
 
